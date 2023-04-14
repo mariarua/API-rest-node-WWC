656f5db1d067664490f3fa16b9130bc70dcbadd9
mariarua <maria.rua1@udea.edu.co>
2023-04-14 13:27:58 -0500

    Create methods get and post

--- v1/controllers/products.controller.js
+++ v1/controllers/products.controller.js
@@ -1,14 +1,40 @@
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+
+const { products } = require("../validators");
+
 const getAllProducts = (req, res) => {
-  products = fs.readFile("archivo.txt", "utf-8", (err, data) => {
+  fs.readFile("archivo.txt", "utf-8", (err, data) => {
     if (err) {
       console.log("error: ", err);
     } else {
-      const objetoJSON = JSON.parse(data);
-      res.json(objetoJSON);
+      const productList = JSON.parse(data);
+      res.status(200).json(productList);
+    }
+  });
+};
+
+const addProduct = (req, res) => {
+  const newId = uuidv4();
+  const product = { id: newId, ...req.body };
+  fs.readFile("archivo.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log("error: ", err);
+    } else {
+      const productList = JSON.parse(data);
+      productList.push(product);
+      console.log(productList);
+      fs.writeFile("archivo.txt", JSON.stringify(productList), (err) => {
+        if (err) {
+          console.error(err);
+        }
+        res.status(200).json(productList);
+      });
     }
   });
 };
 
 module.exports = {
   getAllProducts,
+  addProduct,
 };
