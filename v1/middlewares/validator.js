4260b28fed656e1f095f9ac7271c18a704655bea
mariarua <maria.rua1@udea.edu.co>
2023-04-14 13:28:24 -0500

    Add schema to validator

--- v1/middlewares/validator.js
+++ v1/middlewares/validator.js
@@ -2,13 +2,17 @@ const createHttpError = require("http-errors");
 
 const Validators = require("../validators");
 
-module.exports = function (validator) {
+module.exports = function (validator, schema) {
   if (!Validators.hasOwnProperty(validator))
     throw new Error(`'${validator}' validator is not exist`);
+  if (!Validators[validator].hasOwnProperty(schema))
+    throw new Error(`'${schema}' doesnt exists inside ${validator}`);
 
   return async function (req, res, next) {
     try {
-      const validated = await Validators[validator].validateAsync(req.body);
+      const validated = await Validators[validator][schema].validateAsync(
+        req.body
+      );
       req.body = validated;
       next();
     } catch (err) {
