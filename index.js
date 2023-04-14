1a67dba327e7b1cdca82400130e3a3cb9d69791a
mariarua <maria.rua1@udea.edu.co>
2023-04-13 21:13:38 -0500

    Create get method and set up Joi in project

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+const Routes = require("./routes");
+
 const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+let fs = require("fs");
 
 const PORT = 3000;
 
 const app = express();
 
+// load app middlewares
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// load our API routes
+app.use("/", Routes);
+
 app.get("/", (req, res) => {
   res.send("Esta es mi primera app en express");
+});
+
+app.get("/api/v1/products/", (req, res) => {
+  products = fs.readFile("archivo.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log("error: ", err);
+    } else {
+      const objetoJSON = JSON.parse(data);
+      res.json(objetoJSON);
+    }
+  });
 });
 
 app.listen(PORT, () => {
