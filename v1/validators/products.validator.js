677b69397fabee991b1bf7d37812bb93958431d1
mariarua <maria.rua1@udea.edu.co>
2023-04-14 13:29:25 -0500

    Create Schema to method post and patch

--- v1/validators/products.validator.js
+++ v1/validators/products.validator.js
@@ -1,12 +1,20 @@
 const Joi = require("joi");
 
 const createProductSchema = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string().min(5).required(),
+  price: Joi.number().required(),
+  quantity: Joi.number().positive().required(),
+  category: Joi.string().required(),
+});
+
+const updateProductSchema = Joi.object({
   id: Joi.number().required(),
   name: Joi.string().required(),
   description: Joi.string().min(5).required(),
   price: Joi.number().required(),
-  quantity: Joi.number().positive().require(),
+  quantity: Joi.number().positive().required(),
   category: Joi.string().required(),
 });
 
-module.exports = { createProductSchema };
+module.exports = { createProductSchema, updateProductSchema };
